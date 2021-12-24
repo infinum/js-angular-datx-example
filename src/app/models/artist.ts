884448f0012760a17601efdd4d0a67344c908707
Darko Kukovec <darko@infinum.com>
2021-12-24 11:05:10 +0100

    Add observable model properties

--- src/app/models/artist.ts
+++ src/app/models/artist.ts
@@ -1,9 +1,14 @@
 import { Attribute } from '@datx/core';
+import { Observable } from 'rxjs';
 import { BaseModel } from './base-model';
 
 export class Artist extends BaseModel {
 	public static endpoint = 'artists';
-	public static type = 'project';
+	public static type = 'artist';
+
+	public get $(): { [key in keyof Omit<Artist, keyof BaseModel | '$'>]: Observable<Artist[key]> } {
+		return this._observables as any;
+	}
 
 	@Attribute()
 	public name!: string;
--- src/app/models/base-model.ts
+++ src/app/models/base-model.ts
@@ -1,7 +1,32 @@
-import { IType, Model } from '@datx/core';
 import { jsonapiAngular } from '@datx/jsonapi-angular';
+import { IRawModel, PatchType, PureCollection, IType, Model } from '@datx/core';
+import { getMeta } from '@datx/utils';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 export class BaseModel extends jsonapiAngular(Model) {
+	protected _observables: Record<string, Observable<any>> = {};
+
+	constructor(rawData?: IRawModel, collection?: PureCollection) {
+		super(rawData, collection);
+
+		const fields = getMeta(this, 'fields');
+		Object.keys(fields).forEach((key) => {
+			// @ts-ignore
+			this._observables[key] = new BehaviorSubject(this[key]);
+		});
+		this.onPatch((patch) => {
+			if (patch.patchType === PatchType.UPDATE) {
+				Object.keys(patch.newValue || {}).forEach((key) => {
+					// @ts-ignore
+					console.log('next', key, patch.newValue[key]);
+					// @ts-ignore
+					this._observables[key].next(this[key]);
+				});
+			}
+		});
+		console.log(fields);
+	}
+
 	public get id(): IType {
 		return this.meta.id;
 	}
